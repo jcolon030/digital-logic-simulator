509a56ed9076858cf5c84e72eb294a21a660a415
Jay Colon <jaycolon030@gmail.com>
2026-08-05 09:03:41 -0400

    Basic Header Layout Created

--- app/page.tsx
+++ app/page.tsx
@@ -2,68 +2,44 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div>
+      <div id="header" className="flex text-white gap-5 items-center align-center border-y-3 border-black pb-1">
+        <button className="flex items-center gap-1 border border-gray-600 rounded-md px-2 py-2 shadow-md/50 ml-2">
+          <Image width={25} height={30} className="invert" src="/newfile.svg" alt="newfile"/>
+          <span className="leading-none">New</span>
+        </button>
+        <button className="flex items-center gap-2 border border-gray-600 rounded-md px-2 py-2 shadow-md/50">
+          <Image width={25} height={30} className="invert" src="/openfile.svg" alt="newfile"/>
+          <span>Open</span>
+        </button>
+        <button className="flex items-center gap-2 border border-gray-600 rounded-md px-2 py-2 shadow-md/50">
+          <Image width={25} height={30} className="invert" src="/savefile.svg" alt="newfile"/>
+          <span>Save</span>
+        </button>
+        <button className="flex items-center gap-1 border border-gray-600 rounded-md px-2 py-2 shadow-md/50">
+          <Image width={25} height={30} className="invert" src="/clearfile.svg" alt="newfile"/>
+          <span>Clear</span>
+        </button>
+        <div className="md:size-14 md:grow"></div>
+        <button className="flex items-center gap-2 border border-gray-600 rounded-md px-2 py-2 shadow-md/50">
+          <Image width={25} height={30} className="invert" src="/run.svg" alt="newfile"/>
+          <span>Run</span>
+        </button>
+        <button className="flex items-center gap-2 border border-gray-600 rounded-md px-2 py-2 shadow-md/50">
+          <Image width={25} height={30} className="invert" src="/faq.svg" alt="newfile"/>
+          <span>FAQ</span>
+        </button>
+        <button className="flex items-center gap-1 border border-gray-600 rounded-md px-2 py-2 shadow-md/50 mr-2">
+          <Image width={25} height={30} className="invert" src="/newsettings.svg" alt="newfile"/>
+          <span>Settings</span>
+        </button>
+      </div>
+      <div id="workspace">
+        
+      </div>
+      <div id="footer">
+
+      </div>
     </div>
   );
 }
